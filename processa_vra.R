b8c8ff2601db4946dad459ed4a834e0e2f533db3
eogasawara <eogasawara@eic.cefet-rj.br>
2023-12-22 19:52:45 -0300

    Update

---
+++ processa_vra.R
@@ -0,0 +1,48 @@
+library(readr)
+library(stringr)
+
+col_names <-c("Sigla",
+    "Voo",
+    "DI",
+    "TipoLinha",
+    "AeroportoOrigem",
+    "AeroportoDestino",
+    "PartidaPrevista",
+    "PartidaReal",
+    "ChegadaPrevista",
+    "ChegadaReal",
+    "Situacao",
+    "Justificativa"
+  )
+
+fil <- list.files("vra")
+process <- NULL
+for (f in fil) {
+  filecsv <- sprintf("vra/%s", f)
+  filerdata <- sprintf("vra_rdata/%s", str_replace(f, ".csv", ".rdata"))
+  data <- read_delim(filecsv, delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  col <- ncol(data)
+  if (col == 13) {
+    data[,colnames(data) == "Data Prevista"] <- NULL
+    col <- ncol(data)
+  }
+  if (col == 11) {
+    data$Justificativa <- ""
+    col <- ncol(data)
+  }
+  if (col == 1) {
+    data <- read_delim(filecsv, delim = ",", escape_double = FALSE, trim_ws = TRUE)
+    col <- ncol(data)
+    if (col != 12) {
+      data <- read_delim(filecsv, delim = "\t", escape_double = FALSE, trim_ws = TRUE)
+      col <- ncol(data)
+    }
+  }
+  if (col == 12) {
+    colnames(data) <- col_names
+    save(data, file=filerdata)
+  }
+  process <- rbind(process, data.frame(file = f, col = col))
+}
+ex <- process[process$col != 12, ]
+
